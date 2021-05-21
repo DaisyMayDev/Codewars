b1b937f6c8d29cf9bcc21131d9b6ecce2c9c3aad
DaisyMayDev <49954474+DaisyMayDev@users.noreply.github.com>
2021-05-21 13:59:28 +0100

    Create GetPlanetNameByID.js
    Get Planet Name by ID - The function is not returning the correct values. Can you figure out why?

---
+++ JavaScript/8kyu/GetPlanetNameByID.js
@@ -0,0 +1,39 @@
+// Get Planet Name by ID
+// The function is not returning the correct values. Can you figure out why?
+
+const id = 3;
+
+function getPlanetName(id){
+  var name;
+  switch(id){
+    case 1:
+      name = 'Mercury';
+      break;
+    case 2:
+      name = 'Venus';
+      break;
+    case 3:
+      name = 'Earth';
+      break;
+    case 4:
+      name = 'Mars';
+      break;
+    case 5:
+      name = 'Jupiter';
+      break;
+    case 6:
+      name = 'Saturn';
+      break;
+    case 7:
+      name = 'Uranus';
+      break;
+    case 8:
+      name = 'Neptune';
+      break;
+  }
+  return name;
+}
+
+console.log(`The planet with the ID of ${id} is ${getPlanetName(id)}.`);
+
+//Output in console ->  The planet with the ID of 3 is Earth.
