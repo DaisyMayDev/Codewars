5f4d4d105fd0bc2c6f734d90edfa488ba42f5a0d
DaisyMayDev <49954474+DaisyMayDev@users.noreply.github.com>
2021-06-12 09:55:03 +0100

    Create YouCantCodeUnderPressure.js
    You Can't Code Under Pressure - Code as fast as you can! You need to double the integer and return it.

---
+++ JavaScript/8kyu/YouCantCodeUnderPressure.js
@@ -0,0 +1,12 @@
+//You Can't Code Under Pressure:
+//Code as fast as you can! You need to double the integer and return it.
+
+const i = 3;
+
+function doubleInteger(i) {
+  return i * 2;
+}
+
+console.log(`The number ${i} doubled is ${doubleInteger(i)}.`);
+
+//Output to Console ->  The number 3 doubled is 6.
