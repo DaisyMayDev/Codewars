23dc7b19a0874c5d2dd84ac651cb258e1824b841
DaisyMayDev <49954474+DaisyMayDev@users.noreply.github.com>
2021-06-22 12:07:35 +0100

    Create MultiplyingTwoNumbers.js
    Multiplying Two Numbers - Implement a function which multiplies two numbers.

---
+++ JavaScript/8kyu/MultiplyingTwoNumbers.js
@@ -0,0 +1,13 @@
+//Multiplying Two Numbers:
+//Implement a function which multiplies two numbers.
+
+const num1 = 2;
+const num2 = 5;
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(`${num1} * ${num2} = ${multiply(num1, num2)}`);
+
+//Output to Console ->  2 * 5 = 10
